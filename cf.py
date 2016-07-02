982f1b51f3cf3ff1fd4b10f4a8ef64ea7c38eba2
Anshuman Suri <anshuman14021@iiitd.ac.in>
2016-07-02 23:56:00 +0530

    Rewritten cf.py

--- cf.py
+++ cf.py
@@ -1,50 +1,44 @@
-#!usr/bin/env python
-
-import gi
-gi.require_version('Notify', '0.7')
-from gi.repository import Notify
-from BeautifulSoup import BeautifulSoup as bs
-import urllib2, webbrowser, re, time
-
-names = []
-timings = []
-
-fifteen_minutes = '00:15:00'
-link = 'https://codeforces.com'
-f = urllib2.urlopen(link).read()
-soup = bs(f)
+from bs4 import BeautifulSoup 
+from datetime import datetime, timedelta
+import requests
+import re
+import time
+import subprocess
 
 
-search = re.compile('/contests/*')
-l = []
-s = '[0-9]'
-t = ''
-l.append(s)
-for i in range(6):
-	t = s + t
-	l.append(re.compile(t, re.UNICODE))
-	if i % 2 == 1 :
-		t = ':' + t
+def sendmessage(header,message):
+    subprocess.Popen(['notify-send', header,message])
+    return
 
-contestName = soup.findAll('a', href = search)
-for name in contestName:
-	if 'div' in str(name.parent):
-		x = (name.parent.getText()).replace('Before contest','')
-		for i in range(6):
-			x = re.sub(l[i], '', x)
-		names.append(str(x).strip(':'))
 
-contestTimings = soup.findAll('span', {'class' : 'countdown'})
-i = 0
-for timing in contestTimings:
-	if timing.text <= fifteen_minutes:
-		Notify.init("Codeforces Reminder")
-		notification = Notify.Notification.new(
-			"Codeforces contest",
-			names[i] + ' starts in less than 10 minutes! Taking you to the site...',
-			"/home/hvk/Desktop/favicon.png"
-		)
-		notification.show()
-		i += 1
-		time.sleep(5)
-		webbrowser.open_new(link)
+def get_upcoming_contests(url):
+	soup =  BeautifulSoup(requests.get(url).text, "lxml")
+	tables = soup.findAll("table")
+	upcoming_table = tables[0]
+	rows = upcoming_table.findAll("tr")
+	concerned = []
+	for row in rows[1:]:
+		try:
+			contest_name = row.findAll("td")[0].find(text = True)
+			temp = datetime.strptime(row.findAll("td")[4].find("span").find(text = True), '%H:%M:%S')
+			before_start = timedelta(hours = temp.hour, minutes = temp.minute, seconds = temp.second)
+			concerned.append({"name": contest_name, "time left": before_start})
+		except:
+			continue
+	return concerned
+
+
+def notify_these(contests):
+	fifteen_m = timedelta(minutes = 15)
+	for x in contests:
+		if fifteen_m >= x['time left']:
+			header = 'Codeforces reminder!' 
+			message = x['name'] + "\nStarts in " + str(x['time left'])
+			sendmessage(header, message)
+
+
+if __name__ == "__main__":
+	url = "http://codeforces.com/contests"
+	while True:
+		notify_these(get_upcoming_contests(url))
+		time.sleep(5*60)
